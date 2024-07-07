20dc5aff352a7dd6ef6966a92c4dacef1cb36924
tbosmans <tom.94.bosmans@gmail.com>
2024-07-07 12:08:18 +0200

    chore(backend): setup fastify server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,24 @@
-console.log("hello world")
+import Fastify from "fastify"
+
+async function run() {
+  const app = Fastify()
+
+  app.route({
+    url: "/",
+    method: "GET",
+    handler: async () => {
+      return "hello world"
+    },
+  })
+
+  await app.ready()
+
+  app.listen({ port: 3100, host: "0.0.0.0" }, (err) => {
+    if (!err) return
+
+    app.log.fatal(err)
+    process.exit(1)
+  })
+}
+
+void run()
