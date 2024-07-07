2eb224c570e470759f1149335927ea1caaeb20a3
tbosmans <tom.94.bosmans@gmail.com>
2024-07-07 13:08:17 +0200

    chore(backend): setup awilix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,36 @@
 import Fastify from "fastify"
-import loggerFactory from "./logger/logger.factory"
-import configFactory from "./config/config.factory"
+import loggerFactory, { type Logger } from "./logger/logger.factory"
+import configFactory, { type Config } from "./config/config.factory"
+import { diContainerClassic, fastifyAwilixPlugin } from "@fastify/awilix"
+import { asValue } from "awilix"
 
 async function run() {
   const logger = loggerFactory()
   const config = configFactory()
   const app = Fastify({ logger })
 
+  await app.register(fastifyAwilixPlugin, {
+    injectionMode: "CLASSIC",
+    disposeOnClose: true,
+    disposeOnResponse: true,
+    strictBooleanEnforced: true,
+  })
+
+  diContainerClassic.register({ config: asValue(config) })
+  diContainerClassic.register({ logger: asValue(logger) })
+
   app.route({
     url: "/",
     method: "GET",
-    handler: async () => {
-      return "hello world"
+    handler: async (request) => {
+      const config = request.diScope.resolve<Config>("config")
+      const logger = request.diScope.resolve<Logger>("logger")
+      logger.info("log from the root route")
+      return config.postgres.url
     },
   })
 
   await app.ready()
-
-  app.log.info(`database connection can be found on: ${config.postgres.url}`)
   app.listen({ port: 3100, host: "0.0.0.0" }, (err) => {
     if (!err) return
 
